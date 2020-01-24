7ba4111830adf1069de75d96740aa31db7f53885
Miguel <he591041@C39440L.ens.uqam.ca>
2020-01-24 14:29:59 -0500

    autre commentaires

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,4 +1,5 @@
 public class FizzBuzz {
+	//wow beau code
 	public static void main (String [] args){
 		int max = 100;
 		for(int i= 1; 1 < max; i++){
@@ -6,7 +7,7 @@ public class FizzBuzz {
 				if ( i% 15 == 0)System.out.print("FizzBuzz");
 				else if (i% 3 == 0) System.out.print("Fizz");
 				else if (i% 5 == 0) System.out.print("Buzz");
-				else System.out.print("");
+				else System.out.print(i);
 			
 			
 		}
